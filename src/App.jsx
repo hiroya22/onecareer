e695952221e9287bde30742b3e297c6a0aad9369
Aoyama Kazuki <sekibun3109@gmail.com>
2024-04-21 12:41:10 +0900

    fix: aboutページの修正

--- src/App.jsx
+++ src/App.jsx
@@ -11,34 +11,26 @@ import "./App.css"; // デザインとかはここでcss引っ張ってる
 
 function App() {
     return (
-        <div className="App">
-            <header>
-                <div class="header-left">
-                    <img src={LOGO} className="logo-image" alt="logo"></img>
-                    <Link to="#" class="mypage">マイページ</Link>
-                </div>
-            </header>
-            <main> 
-                <ul> {/*　この2つのリストは画面遷移しやすくするための仮置き。後で消す。 */}
-                    <ol>
-                        <Link to="/">Home</Link>
-                    </ol>
-                    <ol>
-                        <Link to="/about">About</Link> 
-                    </ol> 
-                </ul>
-                {/*　Routesの外は全ページの共通部分、headerとかfooterとかはそろえてよさそう。*/}
-                <Routes> {/*　pathの定義。ページ増やしたかったらここを増やす element=でcomponents内の情報持ってきている。*/}
-                    <Route path="/" element={<Home />} /> 
-                    <Route path="/about" element={<About />} />
-                    <Route path="*" element={<NoMatch />} />
-                </Routes>
-            </main>
-            <footer>
-                <p class="footer-menu">フッター</p>
-            </footer>
-        </div>
-
+      <div className="App">
+        <header>
+          <div class="header-left">
+            <img src={LOGO} className="logo-image" alt="logo"></img>
+            <Link to="https://www.onecareer.jp/my" class="mypage">
+              マイページ
+            </Link>
+          </div>
+        </header>
+        <main>
+          {/*　Routesの外は全ページの共通部分、headerとかfooterとかはそろえてよさそう。*/}
+          <Routes>
+            {" "}
+            {/*　pathの定義。ページ増やしたかったらここを増やす element=でcomponents内の情報持ってきている。*/}
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </main>
+      </div>
     );
 }
 
